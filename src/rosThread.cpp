136a7ac4710985251b7948a23cbbd8b2a6e2b934
hkaraoguz <hkaraoguz@yahoo.com.tr>
2013-07-16 16:46:00 +0300

    initial poziyonlar json dosyasindan okunacak sekilde readInitialPoses fonksiyonu yeniden yazildi.

--- src/rosThread.cpp
+++ src/rosThread.cpp
@@ -20,11 +20,12 @@ RosThread::RosThread()
 void RosThread::work(){
 
 
-    //QString path = QDir::homePath();
 
-   // path.append("/fuerte_workspace/sandbox/initialPoses.txt");
-    QString path;
-    path.append("../initialPoses.txt");
+    QString path = QDir::homePath();
+    path.append("/fuerte_workspace/sandbox/configISL.json");
+
+//    QString path;
+//    path.append("../initialPoses.txt");
     // Read initial poses otherwise quit!!
     if(!readInitialPoses(path))
     {
@@ -187,8 +188,9 @@ void RosThread::handleCoordinatorUpdate(navigationISL::neighborInfo info)
     dataReceived[id] = true;
 
 }
-bool RosThread::readInitialPoses(QString filepath)
+bool RosThread::readInitialPoses(QString filename)
 {
+/*
     QFile file(filepath);
 
     if(!file.open(QFile::ReadOnly))
@@ -216,4 +218,66 @@ bool RosThread::readInitialPoses(QString filepath)
     file.close();
 
     return true;
+*/
+
+    QFile file(filename);
+
+    if(!file.exists()) return false;
+
+    if(!file.open(QFile::ReadOnly)) return false;
+
+    QJson::Parser parser;
+
+    bool ok;
+
+    QVariantMap result = parser.parse(&file,&ok).toMap();
+
+    if(!ok){
+
+        file.close();
+        qDebug()<<"Fatal error - reading initial poses";
+
+        return false;
+    }
+    else
+    {
+        int robotID = result["robotID"].toInt();
+
+        qDebug()<<result["robotID"].toString();
+
+        double initialX = result["initialX"].toDouble();
+
+        double initialY = result["initialY"].toDouble();
+
+
+        // initial pozisyonlar bin'e ataniyor
+
+        bin[robotID][1] = initialX;
+        bin[robotID][2] = initialY;
+
+        qDebug() << "Pose "<< robotID << " " << bin[robotID][1] << " " << bin[robotID][2];
+
+        QVariantMap nestedMap = result["Robots"].toMap();
+
+        foreach (QVariant plugin, nestedMap["Robot"].toList()) {
+
+            QString rNameStr = plugin.toMap()["name"].toString();
+
+            rNameStr.remove("IRobot");
+
+            int rID = rNameStr.toInt();
+
+            bin[rID][1] = plugin.toMap()["initialX"].toDouble();
+            bin[rID][2] = plugin.toMap()["initialY"].toDouble();
+
+
+            qDebug() << "Pose " << rID << " " << bin[rID][1] << " " << bin[rID][2];
+        }
+
+
+
+    }
+    file.close();
+    return true;
+
 }
